7881c15831eb9481c360f70aa19b69c83dd9ac9c
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2024-01-18 20:11:32 -0500

    fixing build error on vercel8

--- src/components/FormElements/index.ts
+++ src/components/FormElements/index.ts
@@ -1,14 +1,19 @@
 import Toggle from "@components/FormElements/Toggle";
 import Form from "@components/FormElements/Form";
 import Table from "@components/FormElements/Table";
-import Select from "@components/FormElements/Select";
+// import Select from "@components/FormElements/Select";
 import Button from "@components/FormElements/Button";
 import Checkbox from "@components/FormElements/Checkbox";
 import FormInput from "@components/FormElements/FormInput";
 import FormLabel from "@components/FormElements/FormLabel";
 import FormField from "@components/FormElements/FormField";
 import FileInput from "@components/FormElements/FileInput";
+import dynamic from "next/dynamic";
 
+const Select = dynamic(
+  () => import("@components/FormElements/Select"),
+  { ssr: false }, // <-- not including this component on server-side
+);
 export {
   Form,
   Select,
